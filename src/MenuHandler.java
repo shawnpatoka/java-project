fa850c88232da5f4d34abdb64cfa9da34acc67ca
Shawn Patoka <accounts@shawnpatoka.com>
2021-11-15 18:03:09 -0600

    refactored menu stuff into its own class

--- src/MenuHandler.java
+++ src/MenuHandler.java
@@ -1,4 +1,12 @@
+import java.util.Scanner;
+
 public class MenuHandler {
+    static Scanner keyboard = new Scanner(System.in);
+
+
+
+    // methods
+
     public static boolean isInt(String userInput) {
         boolean result;
         try {
@@ -15,4 +23,35 @@ public class MenuHandler {
         result = (Integer.parseInt(userInput)) >= min && (Integer.parseInt(userInput)) <= max;
         return result;
     }
+
+
+    public static void runMainMenu(){
+        mainMenuOptions();
+        String userInput = keyboard.nextLine();
+
+        while (!MenuHandler.isInt(userInput) || !MenuHandler.isInRange(userInput, 1,3)) {
+            System.out.println("\n* * * You made an invalid entry, please try again. * * *\n");
+            mainMenuOptions();
+            userInput = keyboard.nextLine();
+        }
+
+        // Redirects based on selection 1-3.
+        if ((Integer.parseInt(userInput))==1) {
+            QuizCreationMode.quizCreationMode();
+        } else if ((Integer.parseInt(userInput))==2) {
+            TestMode.testMode();
+        } else if ((Integer.parseInt(userInput))==3) {
+            System.exit(0);
+        }
+    }
+
+
+    public static void mainMenuOptions(){
+        System.out.println("Welcome to the Quiz Master. Choose from the selection below. Enter a 1-3");
+        System.out.println("1. Manage Questions");
+        System.out.println("2. Test Mode");
+        System.out.println("3. Exit");
+    }
+
+
 }
---
+++ src/SecretWord.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class SecretWord {
+
+    private static boolean isSafe;
+    private static Scanner keyboard = new Scanner(System.in);
+    private static final String SECRET_WORD = "abide";
+
+
+    // methods
+
+    public static boolean getIsSafe() {
+        return isSafe;
+    }
+
+    public void setSafe(boolean safe) {
+        isSafe = safe;
+    }
+
+    public static void runSecretWord(){
+        System.out.println("Enter secret word. Or enter '-1' to exit.");
+        String userInput = keyboard.nextLine();
+
+        if (userInput.equals("-1")) {
+            System.out.println("Exiting Program");
+            System.exit(0);
+        }
+
+        isSafe = SECRET_WORD.equals(userInput);
+
+
+
+    }
+
+
+
+
+
+
+
+}
